0162113776cd943e6724d41f134b30e2c9dea720
natalia <natalia.roman.cruz@gmail.com>
2025-08-07 22:37:32 -0500

    Initial Spring Boot project

---
+++ src/test/java/com/acme/tickit/tickitbackend/TickitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.tickit.tickitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
