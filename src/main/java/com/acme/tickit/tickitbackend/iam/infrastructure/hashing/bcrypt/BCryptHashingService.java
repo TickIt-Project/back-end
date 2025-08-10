45384616ccee51a31d32e18f98e5d32f3ce37eb3
natalia <natalia.roman.cruz@gmail.com>
2025-08-10 00:02:00 -0500

    feat: Add BCryptHashingService dependencies

--- src/main/java/com/acme/tickit/tickitbackend/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
+++ src/main/java/com/acme/tickit/tickitbackend/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
@@ -1,5 +1,8 @@
 package com.acme.tickit.tickitbackend.iam.infrastructure.hashing.bcrypt;
 
+import com.acme.tickit.tickitbackend.iam.application.internal.outboundservices.HashingService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public interface BCryptHashingService extends HashingService, PasswordEncoder {
     String encode(CharSequence rawPassword);
 
