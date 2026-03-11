ab0530df2e34bb7c844641d957feeae75e785c0f
natalia <natalia.roman.cruz@gmail.com>
2026-03-10 23:34:33 -0500

    feat: create statistics history

---
+++ src/main/java/com/acme/tickit/tickitbackend/management/domain/model/valueobjects/IssueStatus.java
@@ -0,0 +1,13 @@
+package com.acme.tickit.tickitbackend.management.domain.model.valueobjects;
+
+public enum IssueStatus {
+    OPEN,
+    TO_DO,
+    IN_PROGRESS,
+    IN_REVIEW,
+    IN_TESTING,
+    QA_REJECTED,
+    ON_HOLD,
+    CLOSED,
+    CANCELLED
+}
--- src/main/java/com/acme/tickit/tickitbackend/troubleshooting/domain/model/valueobjects/Status.java
+++ src/main/java/com/acme/tickit/tickitbackend/troubleshooting/domain/model/valueobjects/Status.java
@@ -2,7 +2,10 @@ package com.acme.tickit.tickitbackend.troubleshooting.domain.model.valueobjects;
 
 public enum Status {
     OPEN,
+    TO_DO,
     IN_PROGRESS,
+    IN_TESTING,
+    QA_REJECTED,
     ON_HOLD,
     CLOSED,
     CANCELLED
